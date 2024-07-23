f07496a2fc2d8ebae957f5cbacfd583ee77bec65
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-07-23 15:42:34 +0900

    [add] サーバー側のデータベース操作クラス追加

---
+++ Server/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name string `json:"Name"`
+}
+
+func main() {
+	db := dbInit()
+
+	initErr := db.AutoMigrate(&User{})
+	if initErr != nil {
+		panic(initErr)
+	}
+
+	// APIフレームワークの初期化
+	r := gin.Default()
+
+	// 接続に対してメッセージを送る
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "接続",
+		})
+	})
+
+	// DBの全データをJSONで返す
+	r.GET("/getAll", func(c *gin.Context) {
+		var users []User
+		db.Find(&users)
+		c.JSON(200, users)
+	})
+
+	r.GET("/get/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var user User
+		db.Select("name").Find(&user, id)
+		c.JSON(200, user)
+	})
+
+	// DBへデータ追加
+	r.POST("/post", func(c *gin.Context) {
+		var user User
+		err := c.BindJSON(&user)
+		if err != nil {
+			panic(err)
+		}
+		db.Create(&user)
+		c.JSON(200, user)
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func dbInit() *gorm.DB {
+	// MariaDBのランキングDBに接続
+	dsn := "okabe:okabe@/RANKING"
+	// mySqlのドライバの初期化
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	return db
+}
