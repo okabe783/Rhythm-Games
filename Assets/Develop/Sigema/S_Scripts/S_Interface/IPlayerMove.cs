bd8cf7892837cd1e2b2bf99f832113f4c6fd1dd5
Shigema <tsubakyun4869@gmail.com>
2024-08-02 17:18:12 +0900

    [remove] 整理前の情報

--- Assets/Develop/Sigema/S_Scripts/S_Interface/IPlayerMove.cs
+++
@@ -1,9 +0,0 @@
-public interface IPlayerMove 
-{
-    /// <summary> 速度 </summary>
-    public float Speed { get; }
-    /// <summary> 上下移動 </summary>
-    public void VerticalMovement();
-    /// <summary> 下降 </summary>
-    public void DownwardMovement();
-}
