960e3f91ef3f45428f8151eb23bb4db506d4cf8c
okabe783 <134473737+okabe783@users.noreply.github.com>
2024-08-09 19:55:45 +0900

    [add]　サウンドの追加

--- Assets/Master/Script/MauseCtrl/MusicCell.cs
+++ Assets/Master/Script/MauseCtrl/MusicCell.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 /// <summary>音楽でデータを表示してセルの位置やイベントを発火する</summary>
 public class MusicCell : FancyCell<MusicItemData,Context>
 {
-    [SerializeField] private Animator _animator;
-    [SerializeField] private Text _txtName; //UIに表示するテキスト
-    [SerializeField] private Button _button;
+    [SerializeField,Header("セルのAnimation")] private Animator _animator;
+    [SerializeField,Header("UIに表示するテキスト")] private Text _txtName;
 
     private string _sceneName; //選択された時に遷移させるシーン
 
@@ -21,19 +20,11 @@ public class MusicCell : FancyCell<MusicItemData,Context>
     //Objectを無効化するとAnimationがリセットされてしまうので現在の位置を再設定して対応する
     private void OnEnable() => UpdatePosition(_currentPos);
 
-    //初期化時に呼び出される
-    public override void Initialize()
-    {
-        //Clickされたときに呼び出す処理を登録
-        _button.onClick.AddListener(() => Context.OnClickSellIndex.Invoke(Index));
-    }
-
     //セルの内容を更新
     public override void UpdateContent(MusicItemData itemData)
     {
         //曲名をLoad
         _txtName.text = itemData.SoundName;
-        //ToDo:ここでDataを取得
         _sceneName = itemData.SelectScene; //読み込むべきシーンをセルに登録
     }
     
@@ -42,8 +33,11 @@ public class MusicCell : FancyCell<MusicItemData,Context>
     {
         _currentPos = position;
         //Animが有効の場合
-        if(_animator.isActiveAndEnabled)
+        if (_animator.isActiveAndEnabled)
+        {
             _animator.Play(AnimatorHash._scroll,-1,position);
+        }
+            
 
         _animator.speed = 0;
     }
