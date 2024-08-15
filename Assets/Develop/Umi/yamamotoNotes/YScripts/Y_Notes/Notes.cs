bbb67a2eb94f176f384d2a96da6f899b0ce4a76c
YamamotoU518 <hw37hnmr@gmail.com>
2024-08-16 00:45:12 +0900

    [Add] perfect,goodならJudgeZoneで消えて、押せなかったときは画面外に出るまで消えないようにした

--- Assets/Develop/Umi/yamamotoNotes/YScripts/Y_Notes/Notes.cs
+++ Assets/Develop/Umi/yamamotoNotes/YScripts/Y_Notes/Notes.cs
@@ -12,5 +12,9 @@ public class Notes : MonoBehaviour
     private void Update()
     {
         transform.position -= transform.right * (_notesManager.NoteSpeed * Time.deltaTime);
+        if (transform.position.x < -10f)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
