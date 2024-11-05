548cefbe8b849b81b3d3fa5d9c1a135d33fb8400
Shigema <tsubakyun4869@gmail.com>
2024-11-05 22:19:16 +0900

    [add] キャラのプレハブ [update] 切り替えたキャラをInGameに反映させた

--- Assets/Master/Script/DataBase/CharacterDataBase.cs
+++ Assets/Master/Script/DataBase/CharacterDataBase.cs
@@ -6,6 +6,7 @@ public class CharacterDataBase : ScriptableObject
 {
     [SerializeField] private Sprite _icon;
     [SerializeField] private Sprite _faceIcon;
+    [SerializeField] private GameObject _character;
     [SerializeField] private int _characterId;
     [SerializeField] private int _hp;
     [SerializeField, Header("スキル名")] private string _skillName;
@@ -13,9 +14,9 @@ public class CharacterDataBase : ScriptableObject
 
     public Sprite Icon => _icon;
     public Sprite FaceIcon => _faceIcon;
+    public GameObject Character => _character;
     public int CharacterId => _characterId;
     public int Hp => _hp;
-
     public string SkillName => _skillName;
     public string SkillInfo => _skillInfo;
 }
