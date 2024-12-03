72a970469d1e402b81b0af6c05f2c2adc66125a9
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-12-04 00:47:01 +0900

    [add] モデルのパラメータ調整、データ保存に関する関数追加

--- Server/go/model/rankingModel.go
+++ Server/go/model/rankingModel.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Ranking struct {
 	gorm.Model
-	HighScore int `json:"HighScore"`
-	Combo     int `json:"Combo"`
-	ItemID    int `json:"ItemID"`
+	HighScore int `json:"highScore"`
+	Combo     int `json:"combo"`
+	ItemID    int `json:"itemID"`
 }
--- Server/go/model/userModel.go
+++ Server/go/model/userModel.go
@@ -1,16 +1,16 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"Server/utils"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"strings"
+)
 
 type User struct {
 	gorm.Model
-	Name string `json:"name"`
-	Pass string `json:"pass"`
-}
-
-type Test struct {
-	gorm.Model
-	Name string `json:"Name"`
+	Name string `gorm:"size:255;not null;unique" json:"name"`
+	Pass string `gorm:"size:255;not null;" json:"pass"`
 }
 
 func (u *User) Save() (User, error) {
@@ -19,4 +19,38 @@ func (u *User) Save() (User, error) {
 		return User{}, err
 	}
 	return *u, nil
+}
+
+func (u *User) BeforeSave() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Pass), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Pass = string(hashedPassword)
+
+	u.Name = strings.ToLower(u.Name)
+
+	return nil
+}
+
+func (u *User) PrepareOutput() User {
+	u.Pass = ""
+	return *u
+}
+
+func GenerateToken(name string, pass string) (string, error) {
+	var user User
+
+	if err := DB.Where("name = ?", name).First(&user).Error; err != nil {
+		return "", err
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Pass), []byte(pass)); err != nil {
+		return "", err
+	}
+
+	t, err := utils.GenerateToken(user.ID)
+
+	return t, err
 }
